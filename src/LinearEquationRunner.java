5b6c5b4ec975376e7435e0bcc963a83c761eca35
1E10 Laptop <sample@bths.edu>
2022-10-17 10:58:54 -0400

    .

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args ) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome!");
+        System.out.print("Enter coordinate 1: ");
+        String firstCoord = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        String secondCoord = scan.nextLine();
+
+
+        //parse int in here
+    }
+}
